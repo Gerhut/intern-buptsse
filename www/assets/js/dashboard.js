0198d54a5a595580f53ca5e0122eb708be6e30a6
trinity <imandry.c@gmail.com>
2013-04-16 15:07:52 +0800

    other attributes in dashboard to variables

--- www/assets/js/dashboard.js
+++ www/assets/js/dashboard.js
@@ -5,7 +5,10 @@
 
     $.get('/view/dashboard.js', function (data) {
         $view = $(eval(data)({
-            photo: "http://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
+            id: "1020394",
+            realname: "XXX",
+            phone: "010-12345678",
+            photo: "http://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50",
         }));
         getView($view);
     }); 
