b240cf03226b2b8dd08d4100129b8d38248cd68e
trinity <imandry.c@gmail.com>
2013-04-16 15:01:48 +0800

    simple loader listen for hash change

--- www/assets/js/loader.js
+++ www/assets/js/loader.js
@@ -1,7 +1,30 @@
 
 !(function (window, $) {
 
+    if ("onhashchange" in window) { // event supported?
+        window.onhashchange = function () {
+            hashChanged(window.location.hash);
+        }
+    }else{ // event not supported:
+        var storedHash = window.location.hash;
+        window.setInterval(function () {
+            if (window.location.hash != storedHash) {
+                storedHash = window.location.hash;
+                hashChanged(storedHash);
+            }
+        }, 100);
+    }
+
+    var hashChanged = function (hash) {
+        var anchor = hash.substring(1);
+        if(anchor) {
+            D(anchor);
+        }
+        return anchor;
+    };
+
     var D = function (script, cb) {
+        $('#content').children().remove();
         $.get('/js/' + script + '.js', function (data) {
             eval(data)($);
             if(cb instanceof Function) cb();
@@ -11,9 +34,7 @@
     window.D = D;
 
     $(document).ready(function () {
-        var anchor = window.location.hash.substring(1);
-        if(anchor) return D(anchor);
-        D('login');
+        if(!hashChanged(window.location.hash)) D('login');
     });
 
 })(window, window.jQuery);
