a4d7d021d3b07b8ea1569e139f8a9e4ac617f497
trinity <imandry.c@gmail.com>
2013-04-17 20:12:16 +0800

    load all position modules and see what will going on

--- www/assets/js/dashboard/position.js
+++ www/assets/js/dashboard/position.js
@@ -6,8 +6,45 @@
 
     $("#task-panel").children().remove();
 
-    $.get('/view/dashboard/position.js', function (data) {
+    $.get('/view/dashboard/position-manage.js', function (data) {
         $view = $(eval(data)());
+        getView($view);
+    });
+
+    $.get('/view/dashboard/position-editor.js', function (data) {
+        $view = $(eval(data)());
+        getView($view);
+    });
+
+    $.get('/view/dashboard/position-apply.js', function (data) {
+        $view = $(eval(data)());
+        getView($view);
+    });
+
+    $.get('/view/dashboard/position-enroll.js', function (data) {
+        $view = $(eval(data)());
+        getView($view);
+    });
+
+    $.get('/view/dashboard/position-view.js', function (data) {
+        $view = $(eval(data)({
+            title: "软件攻城师",
+            department: "宇宙研究部门",
+            need: "20",
+            salary: ">3000",
+            description: "呱……呱呱……",
+            tutor: {
+                name: "刘洋",
+                gender: "先生",
+                title: "首席",
+                phone: "123564326",
+                email: "foo@bar.com"
+            },
+            company: {
+                name: "跨国公司",
+                website: "http://inter.co.jp"
+            },
+        }));
         getView($view);
     });
 
