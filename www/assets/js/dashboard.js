959788ccba9f9fc3f9b2739dd9f384fa4beedc7a
trinity <imandry.c@gmail.com>
2013-04-18 00:31:14 +0800

    mockup for phase change

--- www/assets/js/dashboard.js
+++ www/assets/js/dashboard.js
@@ -8,11 +8,20 @@
         getView($view);
     }); 
 
-    $("#schedule").remove();
-    $.get('/view/schedule.js', function (code) {
-        $view = $(eval(code)(data));
-        $("#task-panel").before($view);
-    }); 
+    var loadSchedule = function () {
+        $("#schedule").remove();
+        $.get('/view/schedule.js', function (code) {
+            $view = $(eval(code)(data));
+            $("#task-panel").before($view);
+            $('button', $view).click(function () {
+                data.phase = parseInt($(this).text());
+                loadSchedule();
+                return false;
+            });
+        }); 
+    };
+
+    loadSchedule();
 
     var getView = function ($view) {
         $view.appendTo("#content");
