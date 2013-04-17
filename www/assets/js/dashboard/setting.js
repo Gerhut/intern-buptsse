a43e4302e0fa8ab69108fa60c494627af67fa8f9
trinity <imandry.c@gmail.com>
2013-04-17 21:37:17 +0800

    setting page

--- www/assets/js/dashboard/setting.js
+++ www/assets/js/dashboard/setting.js
@@ -7,7 +7,9 @@
     $("#task-panel").children().remove();
 
     $.get('/view/dashboard/setting.js', function (data) {
-        $view = $(eval(data)());
+        $view = $(eval(data)({
+            photo: "http://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
+        }));
         getView($view);
     });
 
