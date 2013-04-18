a811c0cc11d2158ea98f27b8e6747c9d00654920
trinity <imandry.c@gmail.com>
2013-04-18 10:23:15 +0800

    enable control of dashboard navigation items

--- www/assets/js/dashboard.js
+++ www/assets/js/dashboard.js
@@ -6,9 +6,15 @@
     $.get('/view/dashboard.js', function (code) {
         $view = $(eval(code)(data));
         getView($view);
+        loadSchedule();
+
     }); 
 
     var loadSchedule = function () {
+        $('#dashboard .nav.nav-list li[id^="nav-"]').addClass('hide');
+        $('#dashboard .nav.nav-list .' + data.role + '.phase' + data.phase).removeClass('hide');
+        $('#dashboard .nav.nav-list .' + data.role + '.phase-all').removeClass('hide');
+
         $("#schedule").remove();
         $.get('/view/schedule.js', function (code) {
             $view = $(eval(code)(data));
@@ -20,8 +26,6 @@
             });
         }); 
     };
-
-    loadSchedule();
 
     var getView = function ($view) {
         $view.appendTo("#content");
